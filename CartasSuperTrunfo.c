53dbf75b57124101f9594a18fdfcca7ecc68313f
Pedro Henrique Barreto Pinto <barretopedrohenrique@gmail.com>
2024-09-06 17:29:58 +0000

    atualização 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,19 +5,177 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
 
-int main() {
+int main(){
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    int código da cidade, número de pontos turísticos;
-    char nome da cidade[70];
-    double população;
-    double PIB;
+    char A1 São Paulo - SP[70] = A1;
+    char A2 Campinas - SP[70] = A2;
+    char A3 Ribeirão Preto - SP[70] = A3;
+    char A4 São José dos Campos - SP[70] = A4;
+    char B1 Florianópolis - SC[70] = B1;
+    char B2 Joinville - SC[70] = B2;
+    char B3 Blumenau - SC[70] = B3;
+    char B4 São José - SC[70] = B4;
+    char C1 Porto Alegre - RS[70] = C1;
+    char C2 Caxias do Sul - RS[70] = C2;
+    char C3 Canoas - RS[70] = C3;
+    char C4 Pelotas - RS[70] = C4;
+    char D1 Rio de Janeiro - RJ[70] = D1;
+    char D2 São Gonçalo - RJ[70] = D2;
+    char D3 Duque de Caxias - RJ[70] = D3;
+    char D4 Nova Iguaçu - RJ[70] = D4;
+    char E1 Curitiba - PR[70] = E1;
+    char E2 Londrina - PR[70] = E2;
+    char E3 Maringá - PR[70] = E3;
+    char E4 Ponta Grossa - PR[70] = E4;
+    char F1 Belo Horizonte - MG[70] = F1;
+    char F2 Uberlândia - MG[70] = F2;
+    char F3 Contagem - MG[70] = F3;
+    char F4 Juiz de fora - MG[70] = F4;
+    char G1 Manaus - AM[70] = G1;
+    char G2 Itacoatiara - AM[70] = G2;
+    char G3 Manacapuru - AM[70] = G3;
+    char G4 Parintins - AM[70] = G4;
+    char H1 Salvador - BA[70] = H1;
+    char H2 Feira de Santana - BA[70] = H2;
+    char H3 Vitória da Conquista - BA[70] = H3;
+    char H4 Camaçari - BA[70] = H4;
+    int número de pontos turísticos A1;
+    int número de pontos turísticos A2;
+    int número de pontos turísticos A3;
+    int número de pontos turísticos A4;
+    int número de pontos turísticos B1;
+    int número de pontos turísticos B2;
+    int número de pontos turísticos B3;
+    int número de pontos turísticos B4;
+    int número de pontos turísticos C1;
+    int número de pontos turísticos C2;
+    int número de pontos turísticos C3;
+    int número de pontos turísticos C4;
+    int número de pontos turísticos D1;
+    int número de pontos turísticos D2;
+    int número de pontos turísticos D3;
+    int número de pontos turísticos D4;
+    int número de pontos turísticos E1;
+    int número de pontos turísticos E2;
+    int número de pontos turísticos E3;
+    int número de pontos turísticos E4;
+    int número de pontos turísticos F1;
+    int número de pontos turísticos F2;
+    int número de pontos turísticos F3;
+    int número de pontos turísticos F4;
+    int número de pontos turísticos G1;
+    int número de pontos turísticos G2;
+    int número de pontos turísticos G3;
+    int número de pontos turísticos G4;
+    int número de pontos turísticos H1;
+    int número de pontos turísticos H2;
+    int número de pontos turísticos H3;
+    int número de pontos turísticos H4;
+    double população A1;
+    double população A2;
+    double população A3;
+    double população A4;
+    double população B1;
+    double população B2;
+    double população B3;
+    double população B4;
+    double população C1;
+    double população C2;
+    double população C3;
+    double população C4;
+    double população D1;
+    double população D2;
+    double população D3;
+    double população D4;
+    double população E1;
+    double população E2;
+    double população E3;
+    double população E4;
+    double população F1;
+    double população F2;
+    double população F3;
+    double população F4;
+    double população G1;
+    double população G2;
+    double população G3;
+    double população G4;
+    double população H1;
+    double população H2;
+    double população H3;
+    double população H4;
+    double área A1;
+    double área A2;
+    double área A3;
+    double área A4;
+    double área B1;
+    double área B2;
+    double área B3;
+    double área B4;
+    double área C1;
+    double área C2;
+    double área C3;
+    double área C4;
+    double área D1;
+    double área D2;
+    double área D3;
+    double área D4;
+    double área E1;
+    double área E2;
+    double área E3;
+    double área E4;
+    double área F1;
+    double área F2;
+    double área F3;
+    double área F4;
+    double área G1;
+    double área G2;
+    double área G3;
+    double área G4;
+    double área H1;
+    double área H2;
+    double área H3;
+    double área H4;
+    double PIB A1;
+    double PIB A2;
+    double PIB A3;
+    double PIB A4;
+    double PIB B1;
+    double PIB B2;
+    double PIB B3;
+    double PIB B4;
+    double PIB C1;
+    double PIB C2;
+    double PIB C3;
+    double PIB C4;
+    double PIB D1;
+    double PIB D2;
+    double PIB D3;
+    double PIB D4;
+    double PIB E1;
+    double PIB E2;
+    double PIB E3;
+    double PIB E4;
+    double PIB F1;
+    double PIB F2;
+    double PIB F3;
+    double PIB F4;
+    double PIB G1;
+    double PIB G2;
+    double PIB G3;
+    double PIB G4;
+    double PIB H1;
+    double PIB H2;
+    double PIB H3;
+    double PIB H4;
     
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
 
-    printf ()
+    printf("São Paulo - SP %s", A1);
+    printf("digite o nº de pontos turísticos: %d", número de pontos turísticos A1)
+    scanf("%d", &número de pontos turísticos A1)
     
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
